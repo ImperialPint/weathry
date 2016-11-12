9321e78a8be3b1ce2fdaa252a8616134b78d2acf
Andrew Crist <andrew.l.crist@gmail.com>
2016-11-12 15:55:10 -0700

    Test

--- source/index.js
+++ source/index.js
@@ -1,3 +1,8 @@
-require('./components/app');
-require('./reset');
-require('./styles');
+import Main from './components/app.jsx';
+import React from 'react';
+require('./components/app.jsx');
+require('./reset.css');
+require('./styles.css');
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<Main />, document.getElementById('application'));
